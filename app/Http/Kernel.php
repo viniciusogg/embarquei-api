2eb3477f53d645d9f5e23a2dc5eb5b60f7719613
Vinícius Gomes <vinicius.gouveia.gomes@gmail.com>
2019-01-27 01:32:37 -0300

    Correção da configuração do cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,13 +14,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+//        \Barryvdh\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-//        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -40,9 +40,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Barryvdh\Cors\HandleCors::class,
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class
         ],
     ];
 
